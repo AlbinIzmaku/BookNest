8048e0d294b239892ea621b56f954c9aed22096f
Albin Izmaku <albinizmaku2@gmail.com>
2026-02-06 14:34:20 +0100

    Added dotenv package and moved port to .env file

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { PORT } from "./config.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+console.log(app);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
