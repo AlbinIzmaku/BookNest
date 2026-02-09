65f31b75722d76629d472de4efef8112ad5436ce
Albin Izmaku <albinizmaku2@gmail.com>
2026-02-09 20:36:53 +0100

    Added delete method

--- backend/src/controllers/booksController.js
+++ backend/src/controllers/booksController.js
@@ -53,3 +53,19 @@ export async function updateBookById(req, res) {
     res.status(500).send({ message: "Interval server error" });
   }
 }
+
+export async function deleteBookById(req, res) {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findByIdAndDelete(id);
+    if (!book) {
+      res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting book");
+    res.status(500).send({ message: "Interval server error" });
+  }
+}
--- backend/src/routes/booksRoutes.js
+++ backend/src/routes/booksRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createBook,
+  deleteBookById,
   getAllBooks,
   getBookById,
   updateBookById,
@@ -12,5 +13,6 @@ router.get("/", getAllBooks);
 router.post("/", createBook);
 router.get("/:id", getBookById);
 router.put("/:id", updateBookById);
+router.delete("/:id", deleteBookById);
 
 export default router;
