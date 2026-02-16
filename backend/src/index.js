8a4ed83943022e9a6a028fae3b51b7cdd0e7d0f9
Albin Izmaku <albinizmaku2@gmail.com>
2026-02-16 15:33:30 +0100

    Added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import Book from "./models/bookModel.js";
 import booksRoutes from "./routes/booksRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -11,47 +12,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
 app.use("/books", booksRoutes);
-
-// app.post("/books", async (req, res) => {
-//   try {
-//     if (!req.body.title || !req.body.author || !req.body.publishYear) {
-//       return res.status(400).send({
-//         message: "Send all required fields: title, author, publishYear",
-//       });
-//     }
-
-//     const newBook = {
-//       title: req.body.title,
-//       author: req.body.author,
-//       publishYear: req.body.publishYear,
-//     };
-
-//     const book = await Book.create(newBook);
-
-//     return res.status(201).send(book);
-//   } catch (error) {
-//     console.error(error.message);
-//     response.status(500).send({ message: error.message });
-//   }
-// });
-
-// app.get("/books", async (req, res) => {
-//   try {
-//     const books = await Book.find({});
-//     return res.status(200).json({
-//       count: books.length,
-//       data: books,
-//     });
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).send({ message: error.message });
-//   }
-// });
 
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
